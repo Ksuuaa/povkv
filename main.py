a93becb4af3e6402273bb4ec2d023c1decb697b5
Ксения В. Новосвит <novosvit_kv@Kvantorium69.local>
2023-04-21 16:21:59 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+﻿import telebot;
+
+bot = telebot.TeleBot('5685601369:AAHNIgCQGd3mZ_Qj-heUk8lmaEK4q63WaYI');
+
+
+@bot.message_handler(content_types=['text', 'document', 'audio'])
+def get_text_messages(message):
+    if message.text == "Привет":
+        bot.send_message(message.from_user.id, "Привет, чем я могу тебе помочь?")
+    elif message.text == "/help":
+        bot.send_message(message.from_user.id, "Напиши привет")
+    else:
+        bot.send_message(message.from_user.id, "Я тебя не понимаю. Напиши /help.")
+bot.polling(none_stop=True, interval=0)
